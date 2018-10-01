199ec762a479d2654901e10d801f6c32199616c0
Yael Chavoya <realidadvirtualwm@gmail.com>
2018-10-01 16:30:46 -0500

    Ejercicio papelería parte 2

---
+++ ejercicios-resueltos/04-papeleria-parte-2.js
@@ -0,0 +1,25 @@
+// Duplicar la cantidad de todos los documentos con cantidad ≤ 50.
+db.inventario.updateMany({
+    cantidad: { $lte: 50 }
+},{
+    $mul: { cantidad: 2 }    
+})
+
+// Renombrar tamaño como medidas en todos los documentos.
+db.inventario.updateMany({},{
+    $rename: { tamaño: "medidas" }
+})
+
+// Reemplazar el primer documento con estado D por un objeto vacío.
+db.inventario.replaceOne({ estado: "D" },{})
+
+//  Actualizar el primer documento que tenga un arreglo en colores
+// y agregarle "gris" a éste.
+db.inventario.updateOne({
+    colores: {$type: "array"}
+}, {
+    $push: { colores: "gris" }
+})
+
+// Eliminar todos los documentos con estado igual a F.
+db.inventario.deleteMany({ estado: "F" })
